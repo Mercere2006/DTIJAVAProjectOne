924f887af9bef65295a621e91db15589c63d680f
Mercere2006 <sry.poilrn@gmail.com>
2025-01-14 15:10:41 +0700

    v.1: create project and MyClass00 na

---
+++ src/com/sau/tech/MyClass00.java
@@ -0,0 +1,9 @@
+package com.sau.tech;
+
+public class MyClass00 {
+    //จะรันได้ต้องมีเมธอด main
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hi, World!");
+    }
+}
